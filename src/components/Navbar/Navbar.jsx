077f380af83f2f35a772b017f152584d6a1b6041
Shawn Lin <>
2018-11-10 01:39:41 -0500

    Fix navbar mobile and desktop selected sync.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,19 @@ import {Link} from 'react-router-dom';
 import Logo from '../../assets/images/logo.png'
 import LogoSquare from '../../assets/images/logo_square.png'
 
-class DesktopNavbar extends Component {
+class NavbarAuto extends Component {
   state = {}
 
   hideFixedMenu = () => this.setState({ fixed: false })
   showFixedMenu = () => this.setState({ fixed: true })
+
+  handlePusherClick = () => {
+    const { sidebarOpened } = this.state
+
+    if (sidebarOpened) this.setState({ sidebarOpened: false })
+  }
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -16,6 +24,7 @@ class DesktopNavbar extends Component {
     const { children } = this.props
 
     return (
+      <>
       <Responsive minWidth={Responsive.onlyTablet.minWidth}>
         <Visibility
           once={false}
@@ -83,27 +92,7 @@ class DesktopNavbar extends Component {
 
         {children}
       </Responsive>
-    )
-  }
-}
 
-class MobileNavbar extends Component {
-  state = {}
-
-  handlePusherClick = () => {
-    const { sidebarOpened } = this.state
-
-    if (sidebarOpened) this.setState({ sidebarOpened: false })
-  }
-
-  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { children } = this.props
-
-    return (
       <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
         <Sidebar.Pushable>
           <Sidebar as={Menu} animation='overlay' vertical visible={this.state.sidebarOpened}>
@@ -177,14 +166,89 @@ class MobileNavbar extends Component {
           </Sidebar.Pusher>
         </Sidebar.Pushable>
       </Responsive>
+      
+      <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='overlay' vertical visible={this.state.sidebarOpened}>
+              <Menu.Item>
+                <img src={Logo} alt="Girls Enterprise Logo" style={{height: "74px", width: "188px"}}/>
+              </Menu.Item>
+
+              <Menu.Item
+                name='home'
+                active={this.state.activeItem === 'home'}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/"
+              >
+              Home
+              </Menu.Item>
+
+              <Menu.Item
+                name='about'
+                active={this.state.activeItem === 'about'}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/about"
+              >
+              About
+              </Menu.Item>
+
+              <Menu.Item
+                name='programmes'
+                active={this.state.activeItem === 'programmes'}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/programmes"
+              >
+              Programmes
+              </Menu.Item>
+
+              <Menu.Item
+                name='volunteer'
+                active={this.state.activeItem === 'volunteer'}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="volunteer"
+              >
+              Volunteer
+              </Menu.Item>
+            </Sidebar>
+
+          <Sidebar.Pusher
+            dimmed={this.state.sidebarOpened}
+            onClick={this.handlePusherClick}
+            style={{ minHeight: '100vh' }}
+          >
+            <Segment
+              textAlign='center'
+              vertical
+            >
+              <Container>
+                <Menu secondary size='small'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <img src={LogoSquare} alt="Girls Enterprise Logo"/>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Responsive>
+
+      </>
     )
   }
 }
 
 const Navbar = ({ children }) => (
   <>
-    <DesktopNavbar>{children}</DesktopNavbar>
-    <MobileNavbar>{children}</MobileNavbar>
+    <NavbarAuto>{children}</NavbarAuto>
   </>
 ) 
 

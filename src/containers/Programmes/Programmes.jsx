9ef65eac9da6b6077c6f5893772be7b60e4d9275
Shawn Lin <>
2018-11-10 04:15:16 -0500

    Fix mobile image dimension

--- src/containers/Programmes/Programmes.jsx
+++ src/containers/Programmes/Programmes.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import './Programmes.css'
-import {Segment, Grid, Header} from 'semantic-ui-react'
+import {Responsive, Segment, Grid, Header, Image} from 'semantic-ui-react';
+import "./Programmes.css"
+import Doctors from "../../assets/images/africadoctor.jpg";
+import Classroom from "../../assets/images/africaclassroom.jpg";
+import Children from "../../assets/images/africachildren.jpg";
 
 const TextHeader = {
     fontSize: '2em',
@@ -18,30 +21,50 @@ const TextBody = {
 const Programmes = (props) => {
     return(
         <>
-          <Segment id="healthcarephoto" style={{height: '30em' }} vertical> </Segment>
-
-            <Segment style={{ padding: '6em 0em', minHeight: '30em'}} basic>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <Segment id="healthcarephoto" basic />
+            <Segment style={{ padding: '4em 0em', minHeight: '20em'}} basic>
             <Grid container stackable verticalAlign='middle'>
             <Grid.Row centered>
-                <Grid.Column width={4}>
+                <Grid.Column width={6}>
                     <Header style={TextHeader} as='h1'> Healthcare </Header>
                 </Grid.Column>
-                <Grid.Column width={8} style={TextBody}>
+                <Grid.Column width={10} style={TextBody}>
+                ORWDAP believes that empowered women are the key to transforming communities; we have therefore developed Girls enterprise, a new social enterprise whose is mission is to ensure women have the strength and support to drive this development.
+                </Grid.Column>
+            </Grid.Row>
+            </Grid>
+            </Segment>
+        </Responsive>
+
+        <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+            <Image src={Doctors} width="100%"/>
+            <Segment style={{ padding: '2em 0em'}} basic>
+            <Grid container stackable verticalAlign='middle'>
+            <Grid.Row centered>
+                <Grid.Column width={6}>
+                    <Header style={TextHeader} as='h1'> Healthcare </Header>
+                </Grid.Column>
+                <Grid.Column width={10} style={TextBody}>
                 ORWDAP believes that empowered women are the key to transforming communities; we have therefore developed Girls enterprise, a new social enterprise whose is mission is to ensure women have the strength and support to drive this development.
                 </Grid.Column>
             </Grid.Row>
             </Grid>
             </Segment>
 
-            <Segment id="classroomphoto" style={{height: '30em' }} vertical> </Segment>
+        </Responsive>
 
-            <Segment style={{ padding: '6em 0em', minHeight: '30em'}} basic>
+
+
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <Segment id="classroomphoto" basic />
+            <Segment style={{ padding: '4em 0em', minHeight: '20em'}} basic>
             <Grid container stackable verticalAlign='middle'>
             <Grid.Row centered reversed="computer tablet">
-                <Grid.Column width={4}>
+                <Grid.Column width={6}>
                     <Header style={TextHeader} as='h1'> Education & Training </Header>
                 </Grid.Column>
-                <Grid.Column width={8} style={TextBody}>
+                <Grid.Column width={10} style={TextBody}>
                 Providing a safe space where women can thrive and achieve their full potential is something we believe to be a critical component in women’s development. ORWDAP focusses on developing young women’s skills and education through a variety of platforms:
                 <ul>
                     <li> IT Classes</li>
@@ -53,21 +76,63 @@ const Programmes = (props) => {
             </Grid.Row>
             </Grid>
             </Segment>
-          
-            <Segment id="childrenphoto" style={{height: '30em' }} vertical> </Segment>
+        </Responsive>
 
-            <Segment style={{ padding: '6em 0em', minHeight: '30em'}} basic>
+        <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+            <Image src={Classroom} width="100%"/>
+            <Segment style={{ padding: '2em 0em'}} basic>
+            <Grid container stackable verticalAlign='middle'>
+            <Grid.Row centered reversed="computer tablet">
+                <Grid.Column width={6}>
+                    <Header style={TextHeader} as='h1'> Education & Training </Header>
+                </Grid.Column>
+                <Grid.Column width={10} style={TextBody}>
+                Providing a safe space where women can thrive and achieve their full potential is something we believe to be a critical component in women’s development. ORWDAP focusses on developing young women’s skills and education through a variety of platforms:
+                <ul>
+                    <li> IT Classes</li>
+                    <li> Entrepreneurship Classes</li>
+                    <li> Leadership Classes</li>
+                    <li> Accessible online courses via Girls Enterprise</li>
+                </ul>
+                </Grid.Column>
+            </Grid.Row>
+            </Grid>
+            </Segment>
+        </Responsive>
+
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <Segment id="childrenphoto" basic />
+
+            <Segment style={{ padding: '4em 0em', minHeight: '20em'}} basic>
             <Grid container stackable verticalAlign='middle'>
             <Grid.Row centered>
-                <Grid.Column width={4}>
+                <Grid.Column width={6}>
                     <Header style={TextHeader} as='h1'> Children </Header>
                 </Grid.Column>
-                <Grid.Column width={8} style={TextBody}>
+                <Grid.Column width={10} style={TextBody}>
                 ORWDAP wants to ensure all members of the community have access to education and healthcare. When children don’t have access to education due to a variety of constraints, our classrooms are open to them for learning and creative workshops.
                 </Grid.Column>
             </Grid.Row>
             </Grid>
           </Segment>
+        </Responsive>
+
+        <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+            <Image src={Children} width="100%"/>
+
+            <Segment style={{ padding: '2em 0em'}} basic>
+            <Grid container stackable verticalAlign='middle'>
+            <Grid.Row centered>
+                <Grid.Column width={6}>
+                    <Header style={TextHeader} as='h1'> Children </Header>
+                </Grid.Column>
+                <Grid.Column width={10} style={TextBody}>
+                ORWDAP wants to ensure all members of the community have access to education and healthcare. When children don’t have access to education due to a variety of constraints, our classrooms are open to them for learning and creative workshops.
+                </Grid.Column>
+            </Grid.Row>
+            </Grid>
+          </Segment>
+        </Responsive>
         </>
     )
 }
